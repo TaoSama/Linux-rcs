9cef8579544a1b907b54bd84cf43cff61f3fbf49
luwentao <luwentao@didichuxing.com>
2017-07-27 19:54:22 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//  Created by TaoSama.
+//  Copyright (c) 2017 TaoSama. All rights reserved.
+//
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10, INF = 0x3f3f3f3f, MOD = 1e9 + 7;
+
+
+
+int main(){
+#ifdef LOCAL
+    freopen("/Users/didi/ACM/in.txt", "r", stdin);
+//  freopen("/Users/didi/ACM/out.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    
+    return 0;
+}
